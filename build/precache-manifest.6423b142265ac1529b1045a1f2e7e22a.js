c635de084a38b059bd342b9d7aa1214d11ae4843
baobaosieudebug <baobaosieudebug@gmail.com>
2021-08-03 18:36:54 +0700

    deploy in heroku

---
+++ build/precache-manifest.6423b142265ac1529b1045a1f2e7e22a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab14954a2bd6ef3917927be16a8b02db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "496bdfa4ef58214cf468",
+    "url": "/static/css/main.1e0d6d27.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496bdfa4ef58214cf468",
+    "url": "/static/js/main.e6b05b80.chunk.js"
+  },
+  {
+    "revision": "ed72fd0300eba1b1f74d",
+    "url": "/static/js/runtime-main.258720a7.js"
+  }
+]);
